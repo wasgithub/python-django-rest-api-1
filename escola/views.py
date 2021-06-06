cd8bad04486e375c872499cf1694ef42e1072cb7
Washington Alexandre da Silva <was.alexandre42@gmail.com>
2021-06-06 19:48:20 -0300

    first commit

---
+++ escola/views.py
@@ -0,0 +1,35 @@
+from rest_framework import viewsets, generics
+from escola.models import Aluno, Curso, Matricula
+from escola.serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasAlunoSerializer, ListaAlunosMatriculadosSerializer
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+class AlunosViewSet(viewsets.ModelViewSet):
+  """Exibindo todos os alunos e alunas"""
+  queryset = Aluno.objects.all()
+  serializer_class = AlunoSerializer
+  authentication_classes = [BasicAuthentication]
+  permission_classes = [IsAuthenticated]
+
+class CursosViewSet(viewsets.ModelViewSet):
+  """"Exibir todos os cursos"""
+  queryset = Curso.objects.all()
+  serializer_class = CursoSerializer
+
+class MatriculaViewSet(viewsets.ModelViewSet):
+  """Listando todas as matrículas"""
+  queryset = Matricula.objects.all()
+  serializer_class = MatriculaSerializer
+
+class ListaMatriculasAluno(generics.ListAPIView):
+  def get_queryset(self):
+    queryset = Matricula.objects.filter(aluno_id=self.kwargs['pk'])
+    return queryset
+  serializer_class = ListaMatriculasAlunoSerializer
+
+class ListaAlunosMatriculados(generics.ListAPIView): 
+  """Listando alunos e alunas matriculados em um curso"""
+  def get_queryset(self):
+    queryset = Matricula.objects.filter(curso_id=self.kwargs['pk'])
+    return queryset
+  serializer_class = ListaAlunosMatriculadosSerializer
